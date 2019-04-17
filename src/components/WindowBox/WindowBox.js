60e0e4283925eff658671db272d6d46a00ed8733
aherco <herco.anthony@gmail.com>
2019-04-17 03:59:50 -0700

    added WindowBox component boilerplate

---
+++ src/components/WindowBox/WindowBox.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+export default class WindowBar extends Component {
+  render() {
+
+    return (
+      <div className='WindowBar'>
+      </div>
+    );
+  }
+}
