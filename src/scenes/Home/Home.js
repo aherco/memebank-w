ce4743e498e5489e80dfd47779f23fa5785a47ed
aherco <herco.anthony@gmail.com>
2019-04-30 20:32:46 -0700

    faq redo

--- src/scenes/Home/Home.js
+++ src/scenes/Home/Home.js
@@ -34,8 +34,9 @@ class Home extends Component {
     return (
       <div className='Home'>
         <TopBar/>
+
 	<WindowBox id='h-home' white='home'>
-		<p>woomfy@memebank $ ~</p>
+	  <p>woomfy@memebank $ ~</p>
 	  <div>
 	    <img alt='' src={process.env.REACT_APP_BOT_ICON}/>
           </div>
@@ -44,7 +45,33 @@ class Home extends Component {
 	    <HomeButton value='invite bot' onClick={this.inviteBot}/>
 	    <HomeButton value='join discord' onClick={this.joinDiscord}/>
 	    <HomeButton value='view public memebank' onClick={this.viewMemebank}/>
+	    <p>~ two dabs and a cat edition ~</p>
           </div>
+	</WindowBox>
+
+	<WindowBox id='h-faq' white='faq'>
+	  <p>woomfy@memebank $ ~</p>
+	  <p id='h-description'>memebank is a bot that makes photo/gif albums from your discord channels</p>
+	  <FAQItem
+	    q='how does it work?'
+	    a='when you post an image/gif in a text channel, the bot will add it to a memebank. mentioning the bot @memebank#6654 will post a link to view the memebank.'
+	  />
+	  <FAQItem
+	    q='what if i want to delete an image/gif from my memebank?'
+	    a='just delete the message containing the image and it will be deleted from memebank.'
+	  />
+	  <FAQItem
+	    q='what if i don`t want a memebank for a specific channel?'
+	    a='you can simply edit the permissions of the bot to restrict it from reading and managing messages for that specific channel.'
+	  />
+	  <FAQItem
+	    q='why doesn`t discord already have this feature?'
+	    a='i really don`t know, but that`s why i made this bot. you`re welcome.'
+	  />
+	  <FAQItem
+	    q='what if my question isn`t answered here?'
+	    a='then hit me up at one of the places listed below.'
+	  />
 	</WindowBox>
       </div>
     );
